ed2ec7203530903ad597c745ce1a7ca352f52e32
Joanne Badua <joannebadua@gmail.com>
2019-06-06 14:51:46 -0700

    cleaned up HTML to keep ID and Class labels consistent, and delete array, and add script src gif.js

--- assets/javascript/GIF.js
+++ assets/javascript/GIF.js
@@ -1,14 +1,23 @@
-$(document).ready(function(){
-    var moves 
+$(document).ready(function () {
+    var list = JSON.parse(localStorage.getItem("queryList"));
 
-$("#find-dancemoves").on("click"), function(event) {
-event.preventDefault();
-}  //on-click listener
-}); //document ready end
+    if(!Array.isArray(list)){
+        list=[];
+    };
+
+//on-click listener to add new buttons
+    $(document).on("click", "#submit-button", function () {
+        event.preventDefault();
+        if ($("#new-button-input".val()){
+            var userInput =$("#new-button-input").val();
+
+        }
+    });
 
 var danceMoves = $("#dance-input").val();
 // grab text from user input
 
+// display what the user types in
 var queryURL = "https://api.giphy.com/v1/gifs/trending?" + danceMoves + "api_key=wslWpWhssAgYDK6zVXacBDsacT47flr4";
 // construct url
 
@@ -16,8 +25,16 @@ var queryURL = "https://api.giphy.com/v1/gifs/trending?" + danceMoves + "api_key
 $.ajax({
     url: queryURL,
     method: "GET"
-}).then(function(response){
+}).then(function (response) {
     // then take the response data
     // and display it in the div with an id of movie-view
-console.log(response)
-})
+$(".dance-image").empty();
+
+    // saving the image_original_url property 
+    var imageURL = response.data.image_original_url;
+    var danceImage = $("<img>");
+danceImage.attr("src", imageUrl);
+danceImage.attr("alt", "dance image");
+$("#gifs-appear-here").prepend(danceImage)
+
+});
